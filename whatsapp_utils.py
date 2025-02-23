ad529154e2a42dce93569d377c414ea95e44a922
OnkarM <om140196@gmail.com>
2025-02-23 19:57:58 +0530

    updated token

--- whatsapp_utils.py
+++ whatsapp_utils.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.INFO)
 
 # WhatsApp API details (for sending messages back)
 WHATSAPP_API_URL = "https://graph.facebook.com/v12.0/510167355521515/messages"
-ACCESS_TOKEN = "EAAJH2BRzadUBO7Cpf0cZATVcFSW1gOt2GvYPTSnxMDI7ufj9sOxsDlAdErmJcAZCTkbDRwZCDi1QsBkxvT1g6O6GqYW6QviJGwg3P9VjY81Ex33qxtZAEVVayuomNMI9mBZBJHLVv9yBgWkUmxQXQckBUo0EWbBbySYegLZB6RhcLFHRrUmP00hRGA63KAwI9ITyHzWUwW7PKZB5J5eWY3TQefQDrASAy3vgDQZD"  
+ACCESS_TOKEN = "EAAJH2BRzadUBO4W1ZB8SKBZC8w2cW4VlJYVOKLBnvngZAdFeRdflAYZC1tqfwEgZAFomqvCHsJ4SkKxz6UVH09Igadyj3gmgONKQxx9rQytFAZB33xXpPW6oUUvwZBLbFHDW2qd9Fh84xsmB3lagrkZCH0C1zn7ChtP68FeV4QvxARg8qdTA7yhZBA9Bjr88q14XSZAky3FtiRUCmUw1otidfSTjgvSKEhZBZArywZAMZD"  
 
 
 def send_whatsapp_message(user_number, message,owner_phone_number):
