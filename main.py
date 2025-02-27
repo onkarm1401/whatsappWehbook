241b6d922eaf9b101fe3910209fe7831378d1c17
OnkarM <om140196@gmail.com>
2025-02-27 19:55:51 +0530

    updated code

--- main.py
+++ main.py
@@ -55,9 +55,7 @@ def whatsapp_webhook(request):
             logger.info(f"updated status is  {updated_status}")
             msg_id = data['entry'][0]['changes'][0]['value']['statuses'][0]['id']
             docs = db.collection("whatsapp-messages").where("msg_id", "==", str(msg_id)).stream()
-            found = False
             for doc in docs:
-                found = True
                 doc.reference.update({
                     'status': updated_status
                 })
